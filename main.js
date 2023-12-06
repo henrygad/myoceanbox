4271168426ed3913da41eba61eb3806c21ed8089
Henry Loveday <henrygad.orji@gmail.com>
2023-12-06 23:12:47 +0100

    versoin 0.2

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-
-
-
 const homebody_el = document.querySelector('body')
+
 const navdropdown_el = document.querySelectorAll('.js-dropdown-nav')
 /* drop down nav function */
-navdropdown_el.forEach( (value, index) => {
-    value.addEventListener('click', e => {
-        value.classList.toggle('drop')
+navdropdown_el.forEach( el => {
+    el.addEventListener('click', e => {
+        el.classList.toggle('drop')
     })
 });
 
 
-/* invoke hero slider */
 
+/* responsove nav js */
+
+const dropnav = (el) =>{
+    el.classList.toggle('drop_nav')
+}
+
+
+const hamburger_el = document.querySelectorAll('.hamburger-js')
+hamburger_el.forEach((el, index) => {
+    el.addEventListener('click', e => {
+        dropnav(el, index)
+        if(index === 1){
+            homebody_el.classList.toggle('body')
+        }
+
+    })
+})
 
 
