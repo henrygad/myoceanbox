60d5bdd87738cc0b17394685c3f35c2ccd222425
Henry Loveday <henrygad.orji@gmail.com>
2023-12-11 16:57:54 +0100

    version 0.7

--- js/product.js
+++ js/product.js
@@ -4,14 +4,16 @@ const products = [
         title: "Open Side (OS) - 40ft HC- New/One Trip - Shipping Container",
         image: "/images/1280x1280/products/1382/13972/side_open__35472.1660033557.jpg?c=2",
         wasprice: "$6,900.00",
-        nowprice: "$6,300.00"
+        nowprice: "$6,300.00",
+        link: "../singleproducts/Buy-40ft-OpenSide-New-Container.html"
     },
     {
         id: 2,
         title: "20' Used Shipping Container - 20ft Storage Containers - Wind &amp; Water Tight (WWT)",
         image: "/images/500x659/products/1834/18515/IMG_1755__01026.1683661262.jpg?c=2",
         wasprice: "$1,300.00",
-        nowprice: "$1,100.00"
+        nowprice: "$1,100.00",
+        link: "../singleproducts/buy-20ft-Storage-Containers-Wind.html"
 
     },
     {
@@ -19,42 +21,48 @@ const products = [
         title: "40' Used Shipping Containers - 40ft Storage Container - Wind &amp; Water Tight (WWT)",
         image: "/images/500x659/products/1254/12674/40_con_31__96197.1657137042.jpg?c=2 ",
         wasprice: " $2,400.00",
-        nowprice: "$2,200.00"
+        nowprice: "$2,200.00",
+        link: "../singleproducts/buy-40ft-Storage-Container.html"
     },
     {
         id: 4,
         title: "Refrigerator Container - 40ft High Cube New / One Trip Reefer Shipping Container",
         image: "/images/500x659/products/1814/18335/4_Front_right_45__68313.1682621572.jpg?c=2",
         wasprice: "$27,900.00",
-        nowprice: "$27,000.00"
+        nowprice: "$27,000.00",
+        link: "../singleproducts/buy-40ft-High-Cube-New-One-Trip-Reefer-Shipping-Container.html"
     },
     {
         id: 5,
         title: "40ft High Cube Used Shipping Container - Cargo Worthy (CW)",
         image: "/images/1280x1280/products/856/8839/20180907_132907_resized_1__75872.1622736199.jpg?c=2",
         wasprice: "$2,800.00",
-        nowprice: " $2,300.00"
+        nowprice: " $2,300.00",
+        link: "../singleproducts/buy-40ft-High-Cube-Used-Shipping-Container.html"
     },
     {
         id: 6,
         title: "40ft High Cube New / One Trip Shipping Container",
         image: "/images/500x659/products/314/8630/40HC_New_picture1__75335.1622498051.jpg?c=2",
         wasprice: "$4,500.00",
-        nowprice: "$4,100.00"
+        nowprice: "$4,100.00",
+        link: "../singleproducts/buy-40ft-High-Cube-New.html"
     },
     {
         id: 7,
         title: "20ft DC New / One Trip Shipping Container",
         image: "/images/500x659/products/317/12857/unnamed_2__13751.1657222039.png?c=2",
         wasprice: "$3,300.00",
-        nowprice: "$2,850.00"
+        nowprice: "$2,850.00",
+        link: "../singleproducts/buy-20ft-DC-New.html"
     },
     {
         id: 8,
         title: "Double Door - 20ft DC New / One Trip Shipping Container",
         image: "/images/500x659/products/419/4987/20-foot-double-door-containers-edit-001__16857.1574694987.jpg?c=2",
         wasprice: "$3,400.00",
-        nowprice: "$3,100.00"
+        nowprice: "$3,100.00",
+        link: "../singleproducts/buy-Double-Door-20ft-DC-New.html"
     }
 ]
 
@@ -66,7 +74,7 @@ products.forEach( value => {
         <li class="product">
             <a href="">
                 <div class="product_img">
-                    <a href="" class="cart_message">Add to Cart</a>
+                    <a href=${value.link} target="_black" class="cart_message">Add to Cart</a>
                     <img src=${value.image}>
                 </div>
             </a>
---
+++ js/reviewslider.js
@@ -0,0 +1,196 @@
+/* start global veribles */
+const reviewslidernavs_el = document.querySelectorAll('.js-review-nav')
+const reviewslidderdotsnav_el = document.querySelectorAll('.js-review-dot-nav')
+const reviewslides_el = document.querySelector('.js-review-slides')
+/* end global veribles */
+
+
+    
+/** start get currect position function **/
+    const RScurrectposition = e => {
+        let RScurrectposition = reviewslides_el.style.left
+        reviewslides_el.style.transition = 'all .5s ease'
+
+        if(RScurrectposition === '0%' || RScurrectposition ===  ''){
+            RScurrectposition = 1
+        
+            /* to the second */
+
+        } else if(RScurrectposition === '-100%') {
+            RScurrectposition = 2
+           
+            /* to the third */
+
+        } else if(RScurrectposition === '-200%'){
+            RScurrectposition = 3 
+          
+            /* to the fourth */
+
+        } else if(RScurrectposition === '-300%'){
+            RScurrectposition = 4
+           
+            /* to the fivth */
+
+        } else if(RScurrectposition === '-400%'){
+            RScurrectposition = 5
+           
+            /* to the sixth */
+
+        } else if(RScurrectposition === '-500%'){
+            reviewslides_el.style.transition = 'none'
+            RScurrectposition = 0
+        
+            /* this will return to number one */
+        }
+        
+          return RScurrectposition;
+    }
+
+/** end get currect position function **/
+
+
+/* start slide fuction*/
+    const RSchangeposition = index => {
+         let math;    
+        RSdisplayarrownav(index)
+        RSdotsnavbackgroundcolor(index)
+
+
+        if (index === 1 || index === 0){
+            math = index * 100
+            reviewslides_el.style.left = '-' + math + '%'
+
+        } else if (index == 2){
+            math = index * 100
+            reviewslides_el.style.left = '-' + math + '%'
+
+        } else if(index === 3){
+            math = index * 100
+            reviewslides_el.style.left = '-' + math + '%'
+
+        } else if (index  === 4){
+            math = index * 100
+            reviewslides_el.style.left = '-' + math + '%'
+
+        } else if(index === 5){
+            math = index * 100
+            RSdotsnavbackgroundcolor(0)
+            reviewslides_el.style.left = '-' + math + '%'
+
+        }
+
+    }
+
+/* end slide fuction*/
+
+
+
+/**  start display arrow nav **/
+    const RSdisplayarrownav = (index) => {
+        reviewslidernavs_el.forEach((el, index2) => {
+            let currect;
+            el.style.display = 'flex'
+
+            if(index === 0 || index === 5){
+                currect  = 0
+
+                if(currect === index2){
+                    el.style.display = 'none'  
+                }
+
+            } else if(index === 4){
+                    currect = 1
+                    if(currect === index2){
+                    el.style.display = 'none' 
+                    }
+            }
+
+        })
+    }
+/**  end display arrow nav **/
+
+
+
+/**  display dots nav background color **/
+    const RSdotsnavbackgroundcolor = index => {
+        reviewslidderdotsnav_el.forEach( (el, index2) => {
+            index2 === index? el.classList.add('black'): el.classList.remove('black')          
+
+        })  
+    }
+/** end display dots nav background color **/
+
+
+
+   
+/** start auto slide functions **/
+    function autoslide() {
+
+        /* auto play */
+        const setIntervalId = setInterval(() => {
+
+        const currect= RScurrectposition()
+        RSchangeposition(currect)
+        }, 5000);
+
+        /* pause auto slide */
+        reviewslides_el.parentElement.addEventListener('mouseover', e => {
+        clearInterval(setIntervalId)
+        })
+
+    }
+
+    /* play auto  slide after paused */
+    reviewslides_el.addEventListener('mouseout', e =>{
+        autoslide()
+    })
+/**  end auto slide functions **/
+
+
+
+/**  start arrow navs slider functions **/
+   /* defind slide move using arrow nav */
+   const RSslidenav = value => { 
+        if(value === 'next'){
+            const currect = RScurrectposition()
+            RSchangeposition(currect)
+        }
+        if(value === 'previous'){
+            const currect = RScurrectposition()
+            RSchangeposition(currect - 2)
+        }
+
+  }
+
+    /* get individual arrow nav control */
+    reviewslidernavs_el.forEach( (el, index) => {
+        el.addEventListener( 'click', e => {
+            if(index === 0) RSslidenav("previous")
+            else if(index === 1) RSslidenav("next")
+
+        })
+
+    })
+/** end arrow navs slider functions **/
+
+
+
+/** start slider dots nav functions **/
+     /* defind slide move using dots nav */
+    const RSslidenavdont = index => {
+         RSchangeposition(index) 
+    }
+
+ 
+    /* get individual dots nav control function */
+    reviewslidderdotsnav_el.forEach( (el, index )=> {
+
+        el.addEventListener('click', e =>{
+            RSslidenavdont(index)
+        })
+    })
+
+/** end slider dots nav functions **/
+
+ window.onload = autoslide();
+
